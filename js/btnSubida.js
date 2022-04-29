73b2925f149f3681bfc0fdda67ffe3095aafb16d
Y4mi3r <carreraarra@gmail.com>
2022-04-29 04:34:47 -0400

    se agrego y modifico las css, nuevas imagenes

--- js/btnSubida.js
+++ js/btnSubida.js
@@ -1,23 +1,23 @@
 addEventListener('DOMContentLoaded', () => {
-    const boton_ir_arriba = document.querySelector('#ir_arriba_boton')
+  const boton_ir_arriba = document.querySelector('#ir_arriba_boton')
 
-    const obtener_pixeles_inicio = () => document.documentElement.scrollTop || document.body.scrollTop
+  const obtener_pixeles_inicio = () => document.documentElement.scrollTop || document.body.scrollTop
 
-    const irArriba = () => {
-        if (obtener_pixeles_inicio() > 0){
-            requestAnimationFrame(irArriba)
-            scrollTo(0, obtener_pixeles_inicio() - (obtener_pixeles_inicio() / 20))
+  const irArriba = () => {
+    if (obtener_pixeles_inicio() > 0){
+        requestAnimationFrame(irArriba)
+        scrollTo(0, obtener_pixeles_inicio() - (obtener_pixeles_inicio() / 20))
         }
     }
 
-    const indicarScroll = () => {
-        if (obtener_pixeles_inicio () > 150) {
-            boton_ir_arriba.className = 'mostrar'
-        } else {
-            boton_ir_arriba.className = 'ocultar'
-        }
+  const indicarScroll = () => {
+    if (obtener_pixeles_inicio () > 150) {
+        boton_ir_arriba.className = 'mostrar'
+    } else {
+        boton_ir_arriba.className = 'ocultar'
+      }
     }
 
-    boton_ir_arriba.addEventListener('click', irArriba)
-    window.addEventListener('scroll', indicarScroll)
+  boton_ir_arriba.addEventListener('click', irArriba)
+  window.addEventListener('scroll', indicarScroll)
 })
